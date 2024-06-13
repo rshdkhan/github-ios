1b46ae90874ce53e1295d75f57c256f177d2246d
Rashid Khan <rashidkhan@elgrocer.com>
2024-06-13 16:14:38 +0500

    init with Xcode template project

---
+++ github-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github-ios
+//
+//  Created by Rashid Khan on 13/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ github-iosUITests/github_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_iosUITestsLaunchTests.swift
+//  github-iosUITests
+//
+//  Created by Rashid Khan on 13/06/2024.
+//
+
+import XCTest
+
+final class github_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
